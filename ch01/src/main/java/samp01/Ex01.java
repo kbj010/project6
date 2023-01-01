4e77e9ce820421c6ceb7a08d81b77a23206c94c0
kbj010 <kbj010@hanmail.net>
2023-01-01 13:53:03 +0900

    2. 또 수정

--- ch01/src/main/java/samp01/Ex01.java
+++ ch01/src/main/java/samp01/Ex01.java
@@ -2,7 +2,7 @@ package samp01;
 // Ex01은 MessageBeanKr을 사용한다
 //   "          "     을 의존한다
 // 좋은 시스템 : 결합도 낮고 응집도는 높은 시스템
-// 첫번째 수정
+
 public class Ex01 {
 	public static void main(String[] args) {
 //		결합도 높은 시스템
