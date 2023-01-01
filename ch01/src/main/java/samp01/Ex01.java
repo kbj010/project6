06e54059b545bbcdf1f8e785b0faec00f7b2af90
kbj010 <kbj010@hanmail.net>
2023-01-01 11:33:33 +0900

    1. 일부 수정

--- ch01/src/main/java/samp01/Ex01.java
+++ ch01/src/main/java/samp01/Ex01.java
@@ -2,12 +2,13 @@ package samp01;
 // Ex01은 MessageBeanKr을 사용한다
 //   "          "     을 의존한다
 // 좋은 시스템 : 결합도 낮고 응집도는 높은 시스템
+// 첫번째 수정
 public class Ex01 {
 	public static void main(String[] args) {
 //		결합도 높은 시스템
 //		MessageBeanKr mbk = new MessageBeanKr();
 		MessageBeanEn mbe = new MessageBeanEn();
 //		mbk.sayHello("바이든");
-		mbe.sayHello("바이든");
+		mbe.sayHello("bydon");
 	}
 }
